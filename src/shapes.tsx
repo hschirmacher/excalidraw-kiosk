1cc197941b74419a1e29d7f1895531c03bb4d9b4
Hartmut Schirmacher <hschirmacher@bht-berlin.de>
2023-03-20 10:59:11 +0100

    removed display of keboard bindings for toolbar

--- src/shapes.tsx
+++ src/shapes.tsx
@@ -16,84 +16,76 @@ export const SHAPES = [
   {
     icon: SelectionIcon,
     value: "selection",
-    key: KEYS.V,
-    numericKey: KEYS["1"],
+    key: null, // KEYS.V,
+    numericKey: null, // KEYS["1"],
     fillable: true,
   },
   {
     icon: RectangleIcon,
     value: "rectangle",
-    key: KEYS.R,
-    numericKey: KEYS["2"],
+    key: null, // KEYS.R,
+    numericKey: null, // KEYS["2"],
     fillable: true,
   },
   {
     icon: DiamondIcon,
     value: "diamond",
-    key: KEYS.D,
-    numericKey: KEYS["3"],
+    key: null, // KEYS.D,
+    numericKey: null, // KEYS["3"],
     fillable: true,
   },
   {
     icon: EllipseIcon,
     value: "ellipse",
-    key: KEYS.O,
-    numericKey: KEYS["4"],
+    key: null, // KEYS.O,
+    numericKey: null, // KEYS["4"],
     fillable: true,
   },
   {
     icon: ArrowIcon,
     value: "arrow",
-    key: KEYS.A,
-    numericKey: KEYS["5"],
+    key: null, // KEYS.A,
+    numericKey: null, // KEYS["5"],
     fillable: true,
   },
   {
     icon: LineIcon,
     value: "line",
-    key: KEYS.L,
-    numericKey: KEYS["6"],
+    key: null, // KEYS.L,
+    numericKey: null, // KEYS["6"],
     fillable: true,
   },
   {
     icon: FreedrawIcon,
     value: "freedraw",
-    key: [KEYS.P, KEYS.X],
-    numericKey: KEYS["7"],
+    key:  null, // [KEYS.P, KEYS.X],
+    numericKey: null, // KEYS["7"],
     fillable: false,
   },
   {
     icon: TextIcon,
     value: "text",
-    key: KEYS.T,
-    numericKey: KEYS["8"],
+    key: null, // KEYS.T,
+    numericKey: null, // KEYS["8"],
     fillable: false,
   },
   {
     icon: ImageIcon,
     value: "image",
     key: null,
-    numericKey: KEYS["9"],
+    numericKey: null, // KEYS["9"],
     fillable: false,
   },
   {
     icon: EraserIcon,
     value: "eraser",
-    key: KEYS.E,
-    numericKey: KEYS["0"],
+    key: null, // KEYS.E,
+    numericKey: null, // KEYS["0"],
     fillable: false,
   },
 ] as const;
 
 export const findShapeByKey = (key: string) => {
-  const shape = SHAPES.find((shape, index) => {
-    return (
-      (shape.numericKey != null && key === shape.numericKey.toString()) ||
-      (shape.key &&
-        (typeof shape.key === "string"
-          ? shape.key === key
-          : (shape.key as readonly string[]).includes(key)))
-    );
-  });
-  return shape?.value || null;
+  // no need for this function
+  return null;
 };
